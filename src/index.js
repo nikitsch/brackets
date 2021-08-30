886cf82f5f727a232eb117e55eebfb819434674d
nikitsch <79355999+nikitsch@users.noreply.github.com>
2021-08-31 00:17:03 +0300

    init: index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let text = str.split('');
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if ( text[i] == bracketsConfig[j][0] && text[i + 1] == bracketsConfig[j][1] ) {
+        text.splice (i, 2);
+        i = -1;
+        continue; 
+      }
+    }
+  }
+  if (text.length == 0) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
